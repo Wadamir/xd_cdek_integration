ca0c3c7e21ac63f4b72dce288e529226c1bdd7dd
Wadamir <domus159@gmail.com>
2026-02-16 19:34:49 +0500

    Initial commit

---
+++ catalog/language/en-gb/extension/shipping/cdek.php
@@ -0,0 +1,6 @@
+<?php
+// Text
+$_['text_title']                = 'Shipping by CDEK';
+$_['text_description']          = 'Shipping by CDEK';
+$_['text_select_pickup_point']  = 'Select pickup point';
+$_['text_delivery_by_courier']  = 'Delivery by courier';
---
+++ catalog/language/en-gb/extension/total/cod_cdek_total.php
@@ -0,0 +1,3 @@
+<?php
+$_['text_title'] = 'Cash on Delivery by CDEK';
+?>
